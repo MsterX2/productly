ac7790088226d7591dd0203f3b55403f09919488
MsterX2 <abellferriol@gmail.com>
2025-07-27 13:28:29 +0200

    Error al mostrar categoría

---
+++ productos/views.py
@@ -0,0 +1,12 @@
+from django.http import HttpResponse
+from django.shortcuts import render
+from .models import Producto, Categoria
+
+
+def index(request):
+    productos = Producto.objects.all().values()
+    return render(
+        request,
+        "index.html",
+        context={"productos": productos}
+    )
